94f59c09cf3d360e095a3f07a75051a763295938
fAROSIA <nate5278@outlook.com>
2018-06-08 15:36:02 +0800

    update 6.8

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -15,8 +14,11 @@
  '(custom-safe-themes
    (quote
     ("c3c0a3702e1d6c0373a0f6a557788dfd49ec9e66e753fb24493579859c8e95ab" default)))
+ '(electric-pair-mode t)
  '(menu-bar-mode nil)
- '(package-selected-packages (quote (better-defaults flycheck py-autopep8 elpy company)))
+ '(package-selected-packages
+   (quote
+    (js2-mode smex material-theme better-defaults flycheck py-autopep8 elpy company)))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
@@ -25,17 +27,32 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inconsolata" :foundry "CYRE" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Source Code Pro" :size 10 :foundry "ADBO" :slant normal :weight bold :width normal))))
+ '(font-lock-comment-face ((t (:foreground "#555555"))))
+ '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
 
-
+;;===========================================================company
 (add-hook 'after-init-hook'global-company-mode)
+;;(global-company-mode t)
+(setq company-idle-delay 0.1
+      company-minimum-prefix-length 1
+      company-require-match nil
+      company-dabbrev-ignore-case nil
+      company-dabbrev-downcase nil
+      company-show-numbers t
+      )
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
 (setq inhibit-startup-message t)
 (setq require-final-newline t)
 (global-linum-mode t)
 (setq-default cursor-type 'bar)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'molokai t)
+(setq make-backup-files nil)
+(setq-default python-indent-offset 4)
 ;;============================================================Python
+(defvar python-indent-guess-indent-offset 4)
 (elpy-enable)
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
@@ -43,3 +60,25 @@
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
+(require 'smex) 
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(yas-reload-all)
+(add-hook 'prog-mode-hook #'yas-minor-mode)
+
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
+;; powerline
+(add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
+(require 'powerline)
+;;(setq powerline-arrow-shape 'arrow)   ;; the default
+;;(setq powerline-arrow-shape 'curve)   ;; give your mode-line curves
+(setq powerline-arrow-shape 'arrow14) ;; best for small fonts
+(setq powerline-color1 "grey22")
+(setq powerline-color2 "grey40")
